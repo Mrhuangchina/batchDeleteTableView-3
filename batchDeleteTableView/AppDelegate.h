770f6d0289177935cb3e976f6a5882ebc5422161
Mrhuangchina <jianmissyoulove@qq.com>
2017-09-01 21:46:55 +0800

    Initial Commit

---
+++ batchDeleteTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  batchDeleteTableView
+//
+//  Created by MrHuang on 17/9/1.
+//  Copyright © 2017年 Mrhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
